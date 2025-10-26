052ebaa54b98557f5931bcbad4e55c6edc46e4ab
MiniSoda17 <isaac.arli@gmail.com>
2025-10-26 17:13:08 +1000

    Removes unnecessary data from dataset

--- recognition/RadiologyGPT_Isaac/dataset.py
+++ recognition/RadiologyGPT_Isaac/dataset.py
@@ -1,7 +1,9 @@
 from datasets import load_dataset
 
 dataset = load_dataset("BioLaySumm/BioLaySumm2025-LaymanRRG-opensource-track")
-
+dataset = dataset.remove_columns(["source", "images_path"])
 print(dataset)
 
-print(dataset["train"][0])
+training_data = dataset["train"]
+validation_data = dataset["validation"]
+test_data = dataset["test"]
